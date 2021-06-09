268a054726d5abb9468aa6ffce95dbf70436478a
Ramya S <82898795+RamyaSenapathy@users.noreply.github.com>
2021-06-09 16:11:57 -0400

    Update week3 assignment.R

--- week3 assignment.R
+++ week3 assignment.R
@@ -1,11 +1,11 @@
-makeCacheMatrix <- function(x= numeric()){      
-  i <- NULL 
+makeCacheMatrix <- function(x= numeric()){          ##getting values   
+  i <- NULL                                         ## i is sit to null
   set <- function(y){ 
           x <<- y        
           i <<- NULL         
         }   
   get <- function() x  
-  setinverse <- function(inverse) i <<- inverse     
+  setinverse <- function(inverse) i <<- inverse        ## changed mean function to inverse function
   getinverse <- function() i 
   
   list(set = set,get = get, 
@@ -19,20 +19,18 @@ makeCacheMatrix <- function(x= numeric()){
 cachesolve  <- function(x, ...) {
   i <- x$getinverse()
   if(!is.null(i)) {
-    message("getting cached data")
+    message("getting cached data")                 ## if already calculated message will get displayed
     return(i)
   }
-  data <- x$get()
-  i <- solve(data, ...)
+  data <- x$get()                                 ## if i is null
+  i <- solve(data, ...)                             ##actual function for inversing the matrix
   x$setinverse(i)
   i
 }
 
 
-test <- makeCacheMatrix(matrix(c(1:2, 2:3),nrow=2,ncol=2))
-
-
-cacheinverse(test)
+test <- makeCacheMatrix(matrix(c(1:2, 2:3),nrow=2,ncol=2))      ## testing the code with matrix values
+cacheinverse(test)                                             ## run twice :  testing if cache value is displayed 
 
 
 
